017aa70e8cd6ef525f62cfc8161041afd2648bd2
jochem pouwels <jjpouwels2004@gmail.com>
2021-04-17 12:55:29 +0200

    started function to properly show monster data

--- APIclasses/creatures.py
+++ APIclasses/creatures.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from APIclasses.creatureData import size, alignment, speed, abilities, sense, action
+from APIclasses.creatureData import size, alignment, speed, walk, abilities, sense, action
 import math
+import tkinter.ttk as ttk
 
 @dataclass
 class creature:
@@ -40,6 +41,33 @@ class creature:
 
     def getCHAbonus(self):
         return math.floor((self.abilities.charisma - 10)/2.0)
+
+    def getAC(self):
+        return self.base_ac
+
+    def show(self, window):
+        frame = ttk.Frame(window)
+
+        ttk.Label(frame, text=f"Name: {self.name}").pack()
+        ttk.Label(frame, text=f"Size: {self.size}").pack()
+        ttk.Label(frame, text=f"Alignment: {self.alignment}").pack()
+        ttk.Label(frame, text=f"AC: {self.getAC()}").pack()
+        ttk.Label(frame, text=f"HP: {self.hp}").pack()
+        ttk.Label(frame, text=f"Hit Dice: {self.hit_dice}").pack()
+        self.showSpeed(frame)
+        ttk.Label(frame, text=f"STR: {self.abilities.strength} - {self.getSTRbonus()}").pack()
+        ttk.Label(frame, text=f"DEX: {self.abilities.dexterity} - {self.getDEXbonus()}").pack()
+        ttk.Label(frame, text=f"CON: {self.abilities.constitution} - {self.getCONbonus()}").pack()
+        ttk.Label(frame, text=f"INT: {self.abilities.intelligence} - {self.getINTbonus()}").pack()
+        ttk.Label(frame, text=f"WIS: {self.abilities.wisdom} - {self.getWISbonus()}").pack()
+        ttk.Label(frame, text=f"CHA: {self.abilities.charisma} - {self.getCHAbonus()}").pack()
+
+        return frame
+
+    def showSpeed(self, window):
+        for s in self.speed:
+            if isinstance(s, walk):
+                ttk.Label(window, text=f"walk speed: {s.value}")
 
 @dataclass
 class monster(creature):
