2f38d13b030c3f05858a4fb53140a18b4788550c
jochem pouwels <jjpouwels2004@gmail.com>
2021-08-28 11:31:42 +0200

    send use action request to server

--- Creatures/CreatureWindow.py
+++ Creatures/CreatureWindow.py
@@ -1,5 +1,5 @@
+from Creatures.CreatureHandler import CreatureHandler
 from Creatures.Actions.ActionWindow import ActionWindow
-from Initiative.InitiativeHandler import InitiativeHandler
 import tkinter.ttk as ttk
 from Creatures.Actions.Action import ActionFactory
 from ScrollableFrame import ScrollableFrame
@@ -7,10 +7,11 @@ from ScrollableFrame import ScrollableFrame
 class CreatureWindow(ttk.Frame):
     actionFactory = ActionFactory()
 
-    def __init__(self, creature: dict, initiativeHandler: InitiativeHandler, master = None, *args, **kwargs):
+    def __init__(self, creature: dict, creatureHandler: CreatureHandler, master = None, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
-        self.initiativeHandler = initiativeHandler
+        self.creature = creature
+        self.creatureHandler = creatureHandler
         ttk.Label(master=self, text=creature['name']).pack()
 
 
@@ -20,9 +21,11 @@ class CreatureWindow(ttk.Frame):
 
         dataNoteBook.add(actionsWindow, text="actions")
         dataNoteBook.pack()
-        for action in creature["actions"]:
+        for action in self.creature["actions"]:
             actionFrame = ActionWindow(
-                self.actionFactory.Create(action), 
+                self.actionFactory.Create(action),
+                self.creature['creatureId'],
+                self.creatureHandler,
                 master = actionsWindow.scrollable_frame)
             actionFrame.pack()
             actionsWindow.bind(actionFrame)
--- Creatures/creatureHandler.py
+++ Creatures/creatureHandler.py
@@ -1,10 +1,19 @@
+from Creatures.Actions.Action import Action
 from ServerHandler.Handler import Handler
 class CreatureHandler(Handler):
 
     def addCreature(self, creatureType: str):
         data = (self.groupName, self.userName, creatureType)
-        print("adding creature", creatureType)
+        print("Adding creature", creatureType)
 
         answer = self.SendServerMessage("addCreature", data)
+
+        return answer[0]
+
+    def UseAction(self, creatureId: int, action: Action):
+        data = (self.groupName, self.userName, creatureId, action)
+        print("Using action", action.name)
+
+        answer = self.SendServerMessage("useAction", data)
 
         return answer[0]
