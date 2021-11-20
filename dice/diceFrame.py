9810761e68c6e12660090650eb8b7804d39d8025
jochem pouwels <jjpouwels2004@gmail.com>
2021-11-20 10:33:50 +0100

    add input to dice rolls

--- dice/diceFrame.py
+++ dice/diceFrame.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import tkinter.ttk as ttk
 
 from DandData.dice import Roll
@@ -9,5 +10,12 @@ class DiceFrame(ttk.Frame):
     def __init__(self, master, roll: Roll) -> None:
         super().__init__(master)
         self.roll = roll
+        self.input = tk.IntVar()
 
         ttk.Label(self, text = self.roll).pack()
+
+        ttk.Entry(self, textvariable = self.input).pack()
+        ttk.Button(self, text = "Apply input", command = self.applyInput).pack()
+
+    def applyInput(self):
+        print(self.input.get())
